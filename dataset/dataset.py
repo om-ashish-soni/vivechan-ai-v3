eef265de7aa91e42ce40c7c31fa3ff8997f7c7f0
om-ashish-soni <oms279400@gmail.com>
2024-04-16 13:15:12 +0000

    updating dataset

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -6,9 +6,9 @@ from config import get_config
 def load_text_dataset():
     load_dotenv()
     # REPLACE WITH YOUR HUGGING FACE ACCOUNT TOKEN ( Go to settings and get access token from hugging face)
-    hf_token = get_config("HF_TOKEN")
+    hf_token = os.getenv("HF_TOKEN")
     # HF_DATASET_CHECKPOINT=om-ashish-soni/vivechan-spritual-text-dataset-v3
-    dataset_checkpoint=get_config("HF_DATASET_CHECKPOINT")
+    dataset_checkpoint=os.getenv("HF_DATASET_CHECKPOINT")
     
     print("Starting dataset loading ........... ",dataset_checkpoint)
     dataset = load_dataset(dataset_checkpoint,token=hf_token)
